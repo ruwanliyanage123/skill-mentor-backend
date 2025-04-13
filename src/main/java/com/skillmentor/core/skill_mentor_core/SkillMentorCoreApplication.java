9a1ef25c98d94232e106c4b2f516f9202ac10e70
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-04-13 13:55:23 +0530

    init commit

---
+++ src/main/java/com/skillmentor/core/skill_mentor_core/SkillMentorCoreApplication.java
@@ -0,0 +1,13 @@
+package com.skillmentor.core.skill_mentor_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillMentorCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillMentorCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillmentor/core/skill_mentor_core/SkillMentorCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillmentor.core.skill_mentor_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillMentorCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
